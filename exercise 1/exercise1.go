72cfb0963974e214e7de58b0f780bf5b3aa52102
firstimedeveloper <hanjun.1998@gmail.com>
2018-05-05 19:27:20 +0900

    finished part 2

--- exercise 1/exercise1.go
+++ exercise 1/exercise1.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"math/rand"
+	"os"
 	"strings"
 	"time"
 )
@@ -25,16 +26,18 @@ func shuffle(records [][]string) [][]string {
 
 func main() {
 
-	iptr := flag.Int("timer", 5, "Time you get for each question, default is 5 s.")
-	bptr := flag.Bool("shuffle", false, "Should the csv file be shuffled?")
+	fileptr := flag.String("csv", "problems.csv", "filename of the csvfile(default is problems.csv)")
+	timeptr := flag.Int("timer", 5, "Time you get for each question, default is 5 s.")
+	shuffleptr := flag.Bool("shuffle", false, "Should the csv file be shuffled?")
 
 	//need to parse the flag variables --spent way too much time trying to figure this out
 	flag.Parse()
 	//reading the csv file
 	//content will be of type []byte
-	content, err := ioutil.ReadFile("problems.csv")
+	content, err := ioutil.ReadFile(*fileptr)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("Unable to open file: %s\n", *fileptr)
+		os.Exit(1)
 	}
 	//stringifies the content from type []byte
 	s := string(content)
@@ -47,30 +50,40 @@ func main() {
 		log.Fatal(err)
 	}
 
-	if *bptr {
+	if *shuffleptr {
 		records = shuffle(records)
 	}
-	fmt.Println("ip: ", *iptr)
+	fmt.Println("ip: ", *timeptr)
 
 	var score int
 	var temp string
 	fmt.Println("Press the Enter key to start the game")
 	fmt.Scanln(&temp)
 	for i := range records {
+		timer := time.NewTimer(time.Second * time.Duration(*timeptr))
+
 		fmt.Printf("%s= ", records[i][0])
-		fmt.Scan(&temp)
-		if score == len(records) {
-			fmt.Println("You Won the game!")
-			break
+		answerCh := make(chan string)
+		// if *scoreptr == len(records) {
+		// 	fmt.Println("You Won the game!")
+		// }
+		go func() {
+			fmt.Scan(&temp)
+			answerCh <- temp
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println("\nTime ranout")
+			fmt.Printf("You got %d question(s) correct out of %d total", score, len(records))
+			return
+		case answer := <-answerCh:
+			if answer == records[i][1] {
+				score++
+			}
 		}
-		if temp == records[i][1] {
-			//fmt.Println("Correct!")
-			score++
-		} else {
-			//fmt.Println("Incorrect! Try again later")
-			//break
-			continue
-		}
+
 	}
 	fmt.Printf("You got %d question(s) correct out of %d total", score, len(records))
+
 }
